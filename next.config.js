32b9dcde9d382e331f9533939c0e7dbecc1fc1be
Bryce Mercines <bryce.mercines@whitecloak.com>
2021-11-01 18:02:55 +0800

    successful boilerplate build

--- next.config.js
+++ next.config.js
@@ -6,5 +6,18 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,  
-  reactStrictMode: true,
+  reactStrictMode: false,
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    })
+    return config
+  },
 });
