8060fb81a0d4c596e969a9e71ce2213c660ea076
Bryce Mercines <bryce.mercines@whitecloak.com>
2021-11-01 20:15:18 +0800

    added in animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,16 @@
-import { useEffect } from "react";
 import Layout from "../components/Layout";
-import { startPageLoad, stopPageLoad, Toast } from "../components/Utils";
 
 const Index = () => {
-  useEffect(() => {
-    startPageLoad();
-    setTimeout(() => {
-      stopPageLoad();
-      Toast("Welcome to Beta");
-    }, 5000);
-  }, []);
+ 
 
   return (
     <>
       <Layout>
         <div className="mast">
          <div className="welcome">
-           <i className="lab la-buffer la-6x shimmer-text"/>
-           <h1 className="shimmer-text">Beta NextJS</h1>
-           <p>Starter Boilerplate for NextJS Web Applications</p>
+           <i className="lab la-buffer la-6x shimmer-text  fade-in-bottom dl-2"/>
+           <h1 className="shimmer-text fade-in-bottom dl-4">Beta NextJS</h1>
+           <p className="fade-in-bottom dl-6">Starter Boilerplate for NextJS Web Applications</p>
          </div>
         </div>
       </Layout>
